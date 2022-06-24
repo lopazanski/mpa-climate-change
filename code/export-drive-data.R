6a4930b7c0666eaefe1e8938d5709569f2b18dd8
lopazanski <cori.lopazanski@gmail.com>
2022-06-24 15:53:35 -0700

    create script to export drive data

---
+++ code/export-drive-data.R
@@ -0,0 +1,91 @@
+# Read and Export MPA Review Data from Drive
+# Cori Lopazanski
+# 24 June 2022
+
+# Setup  ---------------------------------------------------------------------------
+
+# Packages
+library(tidyverse)
+library(googlesheets4)
+
+# Directories
+drive.dir <- "/Volumes/GoogleDrive/My Drive/Research/MPA Climate Change - Conservation International/mpa-climate-change-review"
+data.dir <- here::here("data", "raw-ish")
+      # Called "raw-ish" because some very minimal things are done here.
+      # Deep processing should occur in individual scripts.
+  
+# MPA Plan Search Information -----------------------------------------------
+
+## Read MPA List Data ----
+orig_list <- read_sheet("https://docs.google.com/spreadsheets/d/1kARXHqNtNaDU1E3UxkspOThLWDzuIKaSHqjtFjcBH30/edit#gid=930877353",
+                                sheet = "mpa-list", trim_ws = T)
+extra_list <- read_sheet("https://docs.google.com/spreadsheets/d/1kARXHqNtNaDU1E3UxkspOThLWDzuIKaSHqjtFjcBH30/edit#gid=930877353",
+                         sheet = "extra-op6", trim_ws = T)
+
+## Create Simplified Version ----
+orig_simple <- orig_list %>% select(mpa_id, search, plan_id, notes, name)
+extra_simple <- extra_list %>% select(mpa_id, search, plan_id, notes, name)
+
+## Join Together ----
+list <- rbind(orig_simple, extra_simple)
+
+## Export as RDS ----
+saveRDS(list, file = file.path(data.dir, "exported-mpa-search-list.Rds"))
+
+
+# Plan Metadata -----------------------------------------------------------
+
+## Read plan metadata sheet ----
+plan_metadata <- read_sheet("https://docs.google.com/spreadsheets/d/1lqxaoukY0-k0a5mu5-Zjx-dtHjnPIgYzYkIsnGCudFQ/edit#gid=452132598",
+                       sheet = "mpa-info", trim_ws = T) 
+
+## Simplify -----
+plans <- plan_metadata %>% 
+  select(plan_id:network)
+
+## Export 
+saveRDS(plans, file = file.path(data.dir, "exported-plan-metadata.Rds"))
+
+
+# Data Collection -----------------------------------------------------------
+
+## Read document review ----
+review <- read_sheet("https://docs.google.com/spreadsheets/d/1lqxaoukY0-k0a5mu5-Zjx-dtHjnPIgYzYkIsnGCudFQ/edit#gid=452132598",
+                     sheet = "additional-collection", 
+                     trim_ws = T, 
+                     .name_repair = "minimal", na = "")
+
+review <- as.data.frame(review) 
+
+## Processing ----
+
+# Assign new row names
+new_rows <- c(paste("Q", seq(1:82), sep = "_"))
+rownames(review) <- new_rows
+
+# Extract question key column
+q_key <- data.frame(q_id = new_rows,
+                    q_long = review$`plan-id`)
+
+# Transpose
+review <- as.data.frame(t(review)) %>% 
+  filter(Q_1 != "name") # drop old id row
+
+# Add identifier columns
+data <- review %>% 
+  mutate(plan_id = rep(1:174, each = 2)) %>% 
+  select(plan_id, Q_1:Q_82)
+
+# Remove row names
+rownames(data) = NULL
+
+# Assign NAs to NULL
+data[data == "NULL"] <- NA
+
+
+## Export document review ----
+saveRDS(data, file = file.path(data.dir, "exported-doc-review.Rds"))
+
+## Export question key ----
+saveRDS(q_key, file = file.path(data.dir, "exported-question-key.Rds"))
+
