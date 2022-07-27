4ad196c23cf93a36f01ef5cf5eec7998249473da
lopazanski <cori.lopazanski@gmail.com>
2022-07-27 10:30:13 -0700

    update gitignore, add code

---
+++ code/2-process-document-review.R
@@ -0,0 +1,78 @@
+# Process Document Collection Data 
+# Cori Lopazanski
+# 29 June 2022
+
+# Setup  ----------------------------------------------------------------------
+# Clear workspace
+rm(list = ls())
+
+# Packages
+library(tidyverse)
+library(googlesheets4)
+
+# Directories
+drive.dir <- "/Volumes/GoogleDrive/My Drive/Research/MPA Climate Change - Conservation International/mpa-climate-change-review"
+data.dir <- here::here("data", "raw-ish")
+    # Called "raw-ish" because some very minimal things are done here.
+    # Deep processing should occur in individual scripts (aka this one).
+out.dir <- here::here("data", "processed")
+
+# Read Data -------------------------------------------------------------------
+review <- readRDS(file.path(data.dir, "exported-doc-review.Rds"))
+
+
+
+# Build Data ------------------------------------------------------------------
+
+# Assign new row names
+new_rows <- c(paste("Q", seq(1:82), sep = "_"))
+rownames(review) <- new_rows
+
+# Extract question key column
+q_key <- data.frame(q_id = new_rows,
+                    q_code = review$code,
+                    q_long = review$`plan-id`)
+
+# Transpose
+review_t <- as.data.frame(t(review)) 
+
+# General processing
+data <- review_t %>% 
+  # Drop old ID rows
+  filter(Q_1 != "name") %>% 
+  # Add identifier columns
+  mutate(plan_id = rep(1:174, each = 2)) %>% 
+  select(plan_id, Q_1:Q_82) 
+
+# Remove row names
+rownames(data) = NULL
+
+# Add categories to question key
+key_detailed <- q_key %>% 
+  mutate(q_num = as.numeric(str_remove(q_id, "Q_"))) %>% 
+  mutate(category = case_when(q_num %in% c(1:6, 79:82) ~ "metadata",
+                              q_num %in% c(7:12) ~ "climate",
+                              q_num %in% c(13:30) ~ "objectives",
+                              q_num %in% c(31:33) ~ "assessment",
+                              q_num %in% c(34:46) ~ "design",
+                              q_num %in% c(49:58) ~ "monitoring",
+                              q_num %in% c(61:78) ~ "threats",
+                              q_num %in% c(47, 48, 59, 60) ~ "management"))
+
+# Lengthen review
+review_long <- data %>% 
+  pivot_longer(cols = Q_3:Q_82, 
+               names_to = "q_id",
+               values_to = "entry") %>% 
+  rename(name = Q_1,
+         type = Q_2) %>% 
+  mutate(q_num = as.numeric(str_remove(q_id, "Q_"))) %>% 
+  select(plan_id, name, type, q_id, q_num, entry)
+
+# Match review to key 
+data <- left_join(review_long, key_detailed)
+
+
+# Export processed data -------------------------------------------------------
+saveRDS(data, file = file.path(out.dir, "processed-doc-review.Rds"))
+
---
+++ code/3-build-mpa-area-metadata.R
@@ -0,0 +1,59 @@
+# Process MPA Search List
+# 29 June 2022
+# Cori Lopazanski
+
+
+# Setup  ----------------------------------------------------------------------
+# Clear workspace
+rm(list = ls())
+
+# Packages
+library(tidyverse)
+library(sf)
+
+# Directories
+drive.dir <- "/Volumes/GoogleDrive-105151121202188525604/My Drive/Research/MPA Climate Change - Conservation International/mpa-climate-change-review"
+atlas.dir <- file.path(drive.dir, "atlas-data", "mpatlas_20201223_clean")
+data.dir <- file.path("data", "raw-ish")
+  # Called "raw-ish" because some very minimal things are done here.
+  # Deep processing should occur in individual scripts (aka this one).
+out.dir <- file.path("data", "processed")
+
+# Read Data -------------------------------------------------------------------
+
+## MPA Atlas data
+atlas <- read_sf(dsn = atlas.dir,
+                 layer = "mpatlas_20201223_clean")
+
+## Completed search information
+search <- readRDS(file.path(data.dir, "exported-mpa-search-list.Rds")) %>% 
+  as.data.frame()
+
+## Plan metadata
+plan_data <- readRDS(file.path(data.dir, "exported-plan-metadata.Rds")) %>% 
+  rename(name_plan = name,
+         country_plan = country)
+
+# Process Data ------------------------------------------------------------------
+search_clean <- search %>% 
+  rename(name_search = name) %>% 
+  mutate(plan_id_numeric = if_else(is.numeric(plan_id), plan_id, NA))
+
+
+# Build Data ------------------------------------------------------------------
+
+
+## Filter atlas data for those in our search
+atlas_subset <- atlas %>% 
+  filter(mpa_id %in% search$mpa_id)
+    # Note: There are 3 mpa_id's in the search that are no longer considered 
+    # individual areas in MPA atlas with the updated version. This is because 
+    # the initial search list was generated from an older version of the atlas. 
+    # The 3 id's all correspond to GBR zones, which are represented by other 
+    # mpa_id's and therefore okay to exclude.
+
+## Join atlas data with our search information
+atlas_search <- left_join(atlas_subset, search)
+
+## Join atlas/search combo with plan metadata
+atlas_all <- left_join(atlas_search, plan_data, by = "plan_id")
